f9cc1ed14437f315acf0acdb641360d26cace262
mummhy0811 <mummhy0811@gmail.com>
2024-09-04 17:11:08 +0900

    feat: 2장 코틀린 기초

--- src/main/kotlin/com/example/prac/PracController.kt
+++ src/main/kotlin/com/example/prac/PracController.kt
@@ -1,5 +1,6 @@
 package com.example.prac
 
+import org.apache.coyote.http11.Constants.a
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,5 +12,17 @@ class PracController(private val pracService: pracService) {
         pracService.pracWith()
         return pracService.calcMinutes();
 
+    }
+
+    //0904
+    @GetMapping("/when")
+    fun pracWhen(a:String, b:Int) :String{
+        return pracService.pracWhen(a,b)
+    }
+
+
+    @GetMapping("/when")
+    fun pracFor(a:Int, b:Int) :Int{
+        return pracService.pracFor(a,b)
     }
 }
--- src/main/kotlin/com/example/prac/pracService.kt
+++ src/main/kotlin/com/example/prac/pracService.kt
@@ -44,4 +44,68 @@ class pracService {
         010-1234-5678
          */
     }
+
+    //---------0904
+    fun pracWhen(a:String, b:Int):String{
+
+        //set 인스턴스를 사용해서 비교
+        // 함수가 자주 호출된다면 불필요한 가비지 객체가 늘어남
+
+//        when(setOf(a, b)){
+//            setOf("AM", 0..5) -> return "Good Night!"
+//            setOf("AM", 12) -> return "Good Night!"
+//            setOf("AM", 6..11) -> return "Good Morning!"
+//            setOf("PM", 1..6) -> return "Good Afternoon!"
+//            setOf("PM", 7..11) -> return "Good Night!"
+//            setOf("PM", 12) -> return "Good Afternoon!"
+//            else -> return "wrong input"
+//        }
+
+        return when(setOf(a, b)){
+            setOf("AM", 0..5) -> "Good Night!"
+            setOf("AM", 12) -> "Good Night!"
+            setOf("AM", 6..11) -> "Good Morning!"
+            setOf("PM", 1..6) -> "Good Afternoon!"
+            setOf("PM", 7..11) -> "Good Night!"
+            setOf("PM", 12) -> "Good Afternoon!"
+            else -> "wrong input"
+        }
+
+        //대략 아래와 같은 형태로 바꾸면(직접 비교하면) 추가 객체를 만들지 않음
+        //but, 가독성 떨어짐
+
+//        when {
+//            (a == RED && c2 == YELLOW) ||
+//                    (c1 == YELLOW && c2 == RED) ->
+//                ORANGE
+//
+//            (a == YELLOW && c2 == BLUE) ||
+//                    (c1 == BLUE && c2 == YELLOW) ->
+//                GREEN
+//
+//            (a == BLUE && c2 == VIOLET) ||
+//                    (c1 == VIOLET && c2 == BLUE) ->
+//                INDIGO
+//
+//            else -> throw Exception("Dirty color")
+//        }
+
+
+
+    }
+
+    fun pracFor(a:Int, b:Int):Int{
+        var sum =0
+
+        val bigger = Math.max(a, b)
+        val smaller = Math.min(a, b)
+
+        for(i in bigger downTo smaller step 2){ //큰 수부터 작은 수까지 2씩 감소
+            println(i);
+            sum += i
+        }
+        return sum
+    }
+
+
 }
