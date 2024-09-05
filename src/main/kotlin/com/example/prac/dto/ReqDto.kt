41c3cf9098f9ca28b1b4d0314d9efeeb7cb8872d
mummhy0811 <mummhy0811@gmail.com>
2024-09-05 18:01:42 +0900

    feat: 누락 파일 추가

--- src/main/kotlin/com/example/prac/dto/ReqDto.kt
+++ src/main/kotlin/com/example/prac/dto/ReqDto.kt
@@ -1,9 +1,13 @@
 package com.example.prac.dto
 
+import com.example.prac.domain.UserType
+
+
 //클래스가 데이터를 저장하는 역할만을 수행하면 data class 사용
 
 data class ReqDto (
-    val id: String,
+    val type: UserType,
+    val email: String,
     val pw: String,
     val birth: String?
 )
